43f644310f3bcdf046e268803dc2fb922e316609
riumr <atrium_grate.0j@icloud.com>
2022-07-29 17:32:32 +0900

    220729 실습

--- 3회차/김태형/6_암호문1.py
+++ 3회차/김태형/6_암호문1.py
@@ -1,3 +1,9 @@
 import sys
-
 sys.stdin = open("_암호문1.txt")
+
+# 규칙에 맞게 작성된 명령어를 나열하여 만든 문자열이 주어졌을 때, 암호문을 수정하고, 수정된 결과의 처음 10개 숫자를 출력하는 프로그램을 작성하여라.
+
+N = int(input())
+s = input().split()
+M = int(input())
+commands = input().split()
